9cea7fee2081a6743b5298a46873c921934881bc
Dan Colomb <dancolomb@gmail.com>
2013-02-23 16:45:34 -0600

    Initial Commit

---
+++ AugRealityTest/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  AugRealityTest
+//
+//  Created by Dan Colomb on 2/23/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
